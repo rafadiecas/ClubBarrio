d68f47cde6152acc72319bcdef70dcf37409e036
Rafael <rafadiez35@gmail.com>
2024-04-01 21:13:12 +0200

    primera mezcla

--- ClubBarrioApp/models.py
+++ ClubBarrioApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 
-def Usuario():
+class Usuario(models.Model):
     nombre = models.CharField(max_length=250)
     apellidos = models.CharField(max_length=250)
     password = models.CharField(max_length=250)
