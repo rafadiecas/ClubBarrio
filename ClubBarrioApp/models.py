8c51cf92385726384755e3cca58de7d6c83f3f69
Rafael <rafadiez35@gmail.com>
2024-04-01 21:00:10 +0200

    creacion proyecto

--- ClubBarrioApp/models.py
+++ ClubBarrioApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class categoria(models.Model):
+    tipo = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.tipo
