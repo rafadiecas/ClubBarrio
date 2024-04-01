f0a01ff53199419f763d5d89df26c7055b1e4ab6
Luis <lmarincarmona@safareyes.es>
2024-04-01 21:07:01 +0200

    Primer Commit

--- ClubBarrioApp/models.py
+++ ClubBarrioApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
+
 # Create your models here.
+
+def Usuario():
+    nombre = models.CharField(max_length=250)
+    apellidos = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+class categoria(models.Model):
+    tipo = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.tipo
