ba16bada88208c56db17fd7fefc3fae8ed8b40e0
nahuel <ncastrocaceres@safareyes.es>
2024-04-03 20:37:39 +0200

    Intrducción de barra_navegacion.html en inicio.html, y arreglo en el carrusel del item active con un bucle if. Limitada la lista de noticias en views.py a tres objetos, y ordenada inversamente por id.

--- ClubBarrioApp/views.py
+++ ClubBarrioApp/views.py
@@ -4,5 +4,6 @@ from .models import *
 # Create your views here.
 
 def pagina_inicio(request):
-    list_noticias = Noticias.objects.all()
-    return render(request, 'inicio.html', {'noticias': list_noticias})
+    list_noticias = Noticias.objects.all().order_by('-id')
+    list_noticias = list_noticias[0:3]
+    return render(request, 'inicio.html', {'noticias': list_noticias, 'ultima_noticia': ultima_noticia})
