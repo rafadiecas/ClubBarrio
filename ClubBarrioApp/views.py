5fdbe61348a283a3370ec7df93e18f8f9c7eff6d
Rafael <rafadiez35@gmail.com>
2024-04-03 20:44:34 +0200

    primera version noticia bien

--- ClubBarrioApp/views.py
+++ ClubBarrioApp/views.py
@@ -6,4 +6,4 @@ from .models import *
 def pagina_inicio(request):
     list_noticias = Noticias.objects.all().order_by('-id')
     list_noticias = list_noticias[0:3]
-    return render(request, 'inicio.html', {'noticias': list_noticias, 'ultima_noticia': ultima_noticia})
+    return render(request, 'inicio.html', {'noticias': list_noticias})
