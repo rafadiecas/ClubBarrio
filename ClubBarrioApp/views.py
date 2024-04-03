a47c1bfbb41a315d6d5019f1b7d53be511b1b730
Rafael <rafadiez35@gmail.com>
2024-04-03 18:38:27 +0200

    primer html

--- ClubBarrioApp/views.py
+++ ClubBarrioApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def pagina_inicio(request):
-    return render(request, 'inicio.html')
+    list_noticias = Noticias.objects.all()
+    return render(request, 'inicio.html', {'noticias': list_noticias})
