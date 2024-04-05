53e02e096430c54ed43e64e9e5253fa26c920376
Rafael <rafadiez35@gmail.com>
2024-04-05 19:23:23 +0200

    Noticias con paginador

--- ClubBarrioApp/views.py
+++ ClubBarrioApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import *
+from django.core.paginator import Paginator
+from django.http import Http404
+
 
 # Create your views here.
 
@@ -8,6 +11,20 @@ def pagina_inicio(request):
     list_noticias = list_noticias[0:3]
     return render(request, 'inicio.html', {'noticias': list_noticias})
 
+
 def pagina_noticias(request):
     list_noticias = Noticias.objects.all().order_by('-id')
-    return render(request, 'Noticias.html', {'noticias': list_noticias})
+    page = request.GET.get('page', 1)
+
+    try:
+        paginator = Paginator(list_noticias, 3)
+        list_noticias = paginator.page(page)
+    except:
+        raise Http404
+
+    data = {
+        'entity': list_noticias,
+        'paginator': paginator
+    }
+
+    return render(request, 'Noticias.html', data)
