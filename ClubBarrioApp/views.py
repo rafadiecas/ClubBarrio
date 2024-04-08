ea041b3e7820d8277069da52bc98e7832de2a759
nahuel <ncastrocaceres@safareyes.es>
2024-04-08 18:47:10 +0200

    nueva inserción de noticia

--- ClubBarrioApp/views.py
+++ ClubBarrioApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import *
+from django.core.paginator import Paginator
+from django.http import Http404
+
 
 # Create your views here.
 
@@ -11,3 +14,20 @@ def pagina_inicio(request):
 def login(request):
     return render(request, 'login.html')
 
+
+def pagina_noticias(request):
+    list_noticias = Noticias.objects.all().order_by('-id')
+    page = request.GET.get('page', 1)
+
+    try:
+        paginator = Paginator(list_noticias, 3)
+        list_noticias = paginator.page(page)
+    except:
+        raise Http404
+
+    data = {
+        'entity': list_noticias,
+        'paginator': paginator
+    }
+
+    return render(request, 'Noticias.html', data)
