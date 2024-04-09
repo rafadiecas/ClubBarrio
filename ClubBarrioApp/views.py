0f70bddc081c3ee4a59654141fe2171b8efeb1fc
Rafael <rafadiez35@gmail.com>
2024-04-09 16:46:54 +0200

    intento new usuario

--- ClubBarrioApp/views.py
+++ ClubBarrioApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.core.paginator import Paginator
 from django.http import Http404
@@ -38,3 +38,23 @@ def administrador(request):
 def usuarios(request):
     lista_usuarios = Usuario.objects.all()
     return render(request, 'usuarios.html', {'usuarios': lista_usuarios})
+
+def new_user(request):
+    if request.method == 'GET':
+        Users = Usuario.objects.all()
+        return render(request, "crea_usuario.html", {'Users': Users})
+    else:
+        new = Usuario()
+        new.nombre = request.POST.get('nombre')
+        new.apellidos = request.POST.get('apellidos')
+        new.email = request.POST.get('email')
+        new.password = request.POST.get('password')
+        new.fecha_nacimiento = request.POST.get('fecha_nacimiento')
+        new.save()
+
+    if request.POST.get('rol') == 'Padre':
+        new_padre = TutorLegal()
+        new_padre.usuario_id = new.id
+        new_padre.save()
+
+        return redirect('/ClubBarrioApp/administrador/usuarios/')
