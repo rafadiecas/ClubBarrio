634a1e3b3bb351024b1ed1592d1481df1d923296
Rafael <rafadiez35@gmail.com>
2024-05-16 11:11:47 +0200

    edita entrenador funcional y cambios en foto de la barra navegacion

--- ClubBarrioApp/views.py
+++ ClubBarrioApp/views.py
@@ -505,7 +505,7 @@ def edita_usuario(request, id):
             entrenador = Entrenador.objects.filter(usuario_id=usuario.id)
             return render(request, 'editar_usuarios.html',
                           {'usuario': usuario, 'Equipos': Equipos, 'roles': roles, 'id_equipos': id_equipos,
-                           'Tutores': Tutores})
+                           'Tutores': Tutores, 'datos': entrenador})
         if usuario.rol == 'Tutor':
             tutor = TutorLegal.objects.get(usuario_id=usuario.id)
             tarifas = tarifa.labels
