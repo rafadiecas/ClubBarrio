aad173672c7835b12b2267af0909440ded2e7521
maniribar <nicc030901@gmail.com>
2024-05-28 14:36:39 +0200

    cositas

--- ClubBarrioApp/views.py
+++ ClubBarrioApp/views.py
@@ -1609,3 +1609,60 @@ def borra_pedidos(request, id):
     pedido.delete()
     return redirect('pedidos_listado')
 
+
+# # Total number of books.
+# >>> Book.objects.count()
+# 2452
+#
+# # Total number of books with publisher=BaloneyPress
+# >>> Book.objects.filter(publisher__name="BaloneyPress").count()
+# 73
+#
+# # Average price across all books, provide default to be returned instead
+# # of None if no books exist.
+# >>> from django.db.models import Avg
+# >>> Book.objects.aggregate(Avg("price", default=0))
+# {'price__avg': 34.35}
+#
+# # Max price across all books, provide default to be returned instead of
+# # None if no books exist.
+# >>> from django.db.models import Max
+# >>> Book.objects.aggregate(Max("price", default=0))
+# {'price__max': Decimal('81.20')}
+#
+# # Difference between the highest priced book and the average price of all books.
+# >>> from django.db.models import FloatField
+# >>> Book.objects.aggregate(
+# ...     price_diff=Max("price", output_field=FloatField()) - Avg("price")
+# ... )
+# {'price_diff': 46.85}
+#
+# # All the following queries involve traversing the Book<->Publisher
+# # foreign key relationship backwards.
+#
+# # Each publisher, each with a count of books as a "num_books" attribute.
+# >>> from django.db.models import Count
+# >>> pubs = Publisher.objects.annotate(num_books=Count("book"))
+# >>> pubs
+# <QuerySet [<Publisher: BaloneyPress>, <Publisher: SalamiPress>, ...]>
+# >>> pubs[0].num_books
+# 73
+#
+# # Each publisher, with a separate count of books with a rating above and below 5
+# >>> from django.db.models import Q
+# >>> above_5 = Count("book", filter=Q(book__rating__gt=5))
+# >>> below_5 = Count("book", filter=Q(book__rating__lte=5))
+# >>> pubs = Publisher.objects.annotate(below_5=below_5).annotate(above_5=above_5)
+# >>> pubs[0].above_5
+# 23
+# >>> pubs[0].below_5
+# 12
+#
+# # The top 5 publishers, in order by number of books.
+# >>> pubs = Publisher.objects.annotate(num_books=Count("book")).order_by("-num_books")[:5]
+# >>> pubs[0].num_books
+
+
+
+
+
